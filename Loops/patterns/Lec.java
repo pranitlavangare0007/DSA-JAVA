9e03c52d0b71f34baf83f44caf67d50e55d67d70
Pranit Lavangare <pranitlavangare0007@gmail.com>
2026-04-15 21:46:09 +0530

    patterns practice

---
+++ Loops/patterns/Lec.java
@@ -0,0 +1,18 @@
+package Loops.patterns;
+
+public class Lec {
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++){
+            
+            for(int j=1;j<=(5-i);j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(" "+k);
+            }
+           
+            System.out.println();
+        }
+    }
+    
+}
---
+++ Loops/patterns/RightAlignedNumberPattern.java
@@ -0,0 +1,16 @@
+package Loops.patterns;
+
+public class RightAlignedNumberPattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=(5-i)+1;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
