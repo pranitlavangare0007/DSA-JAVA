ffb9acaf96a3adbde23bb942508c6e96327d1e0b
Pranit Lavangare <pranitlavangare0007@gmail.com>
2026-06-04 22:58:17 +0530

    leetcode problem solved

--- Strings/AddBinary.java
+++ Strings/AddBinary.java
@@ -3,24 +3,30 @@ package Strings;
 public class AddBinary {
     public static String addBinary(String a, String b) {
 
-        StringBuilder sb = new StringBuilder();
+      StringBuilder result = new StringBuilder();
 
-        int i=a.length()-1;
-        int j=b.length()-1;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
 
-        while(i<=0 && j<=0){
- 
-        char carry=' ';
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
 
-        if(a.charAt(i) == '1' && b.charAt(j) == '1'){
-            sb.append('0');
-            carry='1';
-        }else{
-            sb.append('0');
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            result.append(sum % 2);
+            carry = sum / 2;
         }
-        }
-       
-        return sb.toString();
+
+        return result.reverse().toString();
 
     }
 
