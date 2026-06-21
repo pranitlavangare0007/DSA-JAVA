3d8cc7568c8880706bc5027877d23cfb846b7dd4
Pranit Lavangare <pranitlavangare0007@gmail.com>
2026-06-21 23:31:39 +0530

    greedy and string question solved

---
+++ Strings/FizzBuzz.java
@@ -0,0 +1,31 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> list= new ArrayList<>();
+
+        for(int i=1;i<=n;i++){
+            if(i % 3==0 && i % 5 == 0){
+                list.add("FizzBuzz");
+            }else if(i %3 == 0){
+                list.add("Fizz");
+            }else if(i % 5 == 0){
+                list.add("Buzz");
+            }else{
+                list.add(String.valueOf(i));
+            }
+        }
+        
+        return list;
+        
+    }
+    
+    public static void main(String[] args) {
+        int n=3;
+        System.out.println(fizzBuzz(n));
+    }
+}
---
+++ greedyAlgo/MaximumIceCreamBars.java
@@ -0,0 +1,19 @@
+package greedyAlgo;
+
+import java.util.Arrays;
+
+public class MaximumIceCreamBars {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count=0;
+        for(int i=0;i<costs.length;i++){
+            if(costs[i] <= coins){
+                coins -= costs[i];
+                count++;
+            }else{
+                break;
+            }
+        }
+        return count;
+    }
+}
